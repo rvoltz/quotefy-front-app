061b4cb9fc0867ed465a4554bc8b993c20ffa803
Rafael Voltz <rvoltz@gmail.com>
2025-09-16 22:28:46 -0300

    fix: route

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+// NOVO: Importe o HashRouter
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/quotefy-front-app">
+    {/* NOVO: Troque BrowserRouter por HashRouter e remova o basename */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
